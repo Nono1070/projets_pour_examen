1384a5f6eac7b4793d95649ef7e1ed73e82c1597
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-08-24 22:41:03 +0200

    Vue de modification (edit) d'un artiste

--- reservations/urls.py
+++ reservations/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 
     path('artistes/', views.artist_index, name='artist_index'),
     path('artiste/create/', views.artist_create, name='artist_create'),
+    path('artiste/edit/<int:id>/', views.artist_edit, name='artist_edit'),
     path('artist/<int:id>/', views.show_artist, name='show_artist'),
 ]
--- reservations/views.py
+++ reservations/views.py
@@ -46,3 +46,25 @@ def artist_create(request):
     return render(request, 'artist/create.html', {
         'form': form,
     })
+
+
+def artist_edit(request, id):
+    # on récupère l'objet correspondant à l'id passé dans l'URL
+    artist = get_object_or_404(Artist, id=id)
+
+    # on passe l'objet en instance dans le formulaire
+    form = ArtistForm(request.POST or None, instance=artist)
+
+    if request.method == 'POST':
+        method = request.POST.get('_method', '').upper()
+
+        if method == 'PUT':
+            if form.is_valid():
+                form.save()
+
+                return redirect('reservations:show_artist', id=artist.id)
+
+    return render(request, 'artist/edit.html', {
+        'form': form,
+        'artist': artist,
+    })
