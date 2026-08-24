74e141c2ce0f304ec082343dc238e5be19af61cd
Mohamed Ouedarbi <mouedarbi@gmail.com>
2026-08-24 22:57:28 +0200

    Modele UserMeta (profil), admin personnalise et groupes ADMIN/MEMBER

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,21 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
-from .models import Artist, Type, Locality, Location, Show
+from .models import Artist, Type, Locality, Location, Show, UserMeta
+
 
 # Personnalisation de l'affichage pour Artist
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('firstname', 'lastname')# Colonnes à afficher
-    search_fields = ('firstname', 'lastname')# Barre de recherche
+    list_display = ('firstname', 'lastname')  # Colonnes à afficher
+    search_fields = ('firstname', 'lastname')  # Barre de recherche
+
 
 # Personnalisation pour Show
 class ShowAdmin(admin.ModelAdmin):
-    list_display = ('title', 'bookable', 'price')# Colonnes à afficher
-    search_fields = ('title',) # Barre de recherche
-    
+    list_display = ('title', 'bookable', 'price')  # Colonnes à afficher
+    search_fields = ('title',)  # Barre de recherche
+
+
 admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Type)
 admin.site.register(Locality)
 admin.site.register(Location)
 admin.site.register(Show, ShowAdmin)
 
+admin.site.index_title = "Projet Réservations"
+admin.site.index_header = "Projet Réservations"
+admin.site.site_title = "Spectacles"
+
+
+# Ajout des informations de profil (UserMeta) dans la fiche User de Django Admin
+class UserMetaInline(admin.StackedInline):
+    model = UserMeta
+    can_delete = False
+    verbose_name_plural = "user_meta"
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [UserMetaInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- reservations/models/__init__.py
+++ reservations/models/__init__.py
@@ -3,3 +3,4 @@ from .type import *
 from .locality import *
 from .location import *
 from .show import *
+from .user_meta import *
